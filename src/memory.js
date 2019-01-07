0771727bef9b92c1f6d5b25e17b94719f22927c1
Paige Williams and Robert Bruce <robert@>
2019-01-07 14:06:24 -0800

    add shuffle cards feature

--- src/memory.js
+++ src/memory.js
@@ -1,4 +1,19 @@
 import { Card } from './card';
+// import { shuffle } from 'shuffle-array'
+
+Array.prototype.shuffle = function() {
+  var input = this;
+
+  for (var i = input.length-1; i >=0; i--) {
+
+    var randomIndex = Math.floor(Math.random()*(i+1));
+    var itemAtIndex = input[randomIndex];
+
+    input[randomIndex] = input[i];
+    input[i] = itemAtIndex;
+  }
+  return input;
+}
 
 export function Memory() {
   this.difficulty = 10;
@@ -18,15 +33,19 @@ Memory.prototype.render = function() {
     cardNumbers.push(Math.floor(Math.random() * Math.floor(100)));
   }
 
-  var cardNumbers2 = cardNumbers;
+  var cardNumbers2 = cardNumbers.map(x => x * 1);
   var output = "";
+  debugger;
+  cardNumbers.shuffle();
+  console.log("cards1");
+  console.log(cardNumbers);
+  cardNumbers2.shuffle();
+  console.log("cards2");
+  console.log(cardNumbers2);
 
   while (cardNumbers.length > 0){
     output += new Card(cardNumbers.pop()).render();
-  }
-
-  while (cardNumbers2.length > 0) {
-    output += new Card(cardNumbers.pop()).render();
+    output += new Card(cardNumbers2.pop()).render();
   }
 
   return output;
